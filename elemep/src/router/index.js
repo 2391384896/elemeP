f53f5de057687ffa26001a6b1c7f3215e702edbb
DESKTOP-5TBM399\lx <2391384896@qq.com>
2019-10-11 21:51:35 +0800

    index.js更新

--- elemep/src/router/index.js
+++ elemep/src/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Lbody from '../components/TakeOut/Lbody'
+import Ltake from '../components/TakeOut/Ltake'
+import Shouye from '../components/ytg/Shouye'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      
-    }
-  ]
+    {path:"/lbody",component:Lbody},
+    {path:"/ltake",component:Ltake},
+    {path:"/Shouye",component:Shouye}
+  ],
 })
