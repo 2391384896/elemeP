d25a6957896dbf0ad85086a6bd1d08a17eba1864
DESKTOP-5TBM399\lx <2391384896@qq.com>
2019-10-12 09:39:23 +0800

    head

--- elemep/src/components/TakeOut/Lheads.vue
+++ elemep/src/components/TakeOut/Lheads.vue
@@ -1,5 +1,18 @@
 <template>
-    $END$
+     <div id="heads">
+       <mt-header title="标题" class="mt_header">
+         <!--返回搜索页-->
+         <router-link to="{}" slot="left">
+           <mt-button icon="back">搜索</mt-button>
+         </router-link>
+         <!--返回个人中心页-->
+         <router-link to="{}" slot="right">
+           <mt-button icon="more" >个人中心</mt-button>
+         </router-link>
+
+       </mt-header>
+
+     </div>
 </template>
 
 <script>
@@ -9,5 +22,10 @@
 </script>
 
 <style scoped>
-
+#heads{
+  width: 100%;
+}
+.mt_header{
+  height: 2.5rem;
+}
 </style>
