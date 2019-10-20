397467f5d4d658ee8232411c358bca91df667e88
tianyonggen <1368833693>
2019-10-20 16:05:39 +0800

    推送3

--- elemep/src/components/ytg/Tsearch.vue
+++ elemep/src/components/ytg/Tsearch.vue
@@ -5,25 +5,42 @@
       <!--head是头部-->
      <div id="head">
       <img src="../../assets/img_t/zuojian.png"/>
-       <p>搜索</p>
+       <div id="bt">{{address.name}}</div>
 
      </div>
       <div >
-        <input type="text" placeholder="请输入商家或美食名称" id="input">
-        <button id="tj" @click="tijiao()">提交</button>
+        <input type="search" placeholder="请输入商家或美食名称" id="inputs" v-model="aaa">
+        <button id="tj"  @click="tijiao()">提交</button>
       </div>
       <div v-show="classbus" id="business">
         <h4>商家</h4>
-        <router-link :to={} v-for="(p,i) in datas" :key="i">
-          {{p.name}}
-        </router-link>
+        <div  class="shangjia" :to={} v-for="(p,i) in datas" :key="i" @click="sj()">
+
+          <img class="img1" :src="'//elm.cangdu.org/img/'+p.image_path" >
+          <div class="xinxi" @click="cz()">
+            <p class="p1">
+              <span>{{p.name}}</span>
+              <img class="zhifu" src="../../assets/img_t/zhiifu.png" alt="">
+            </p>
+            <p class="p1">
+              月售 {{p.recent_order_num}} 单
+            </p>
+            <p class="p1">{{p.float_minimum_order_amount}}元起送/距离{{p.distance}}</p>
+          </div>
+        </div>
+
       </div>
+      <div v-show="baoqian" id="baoqian">
+        很抱歉，无搜索结果
+      </div>
+      <div class="cha" v-for="(p2,i2) in urL"  v-if="!p2.checked">{{p2.title}}<img src="../../assets/img_t/cha.png"  @click="removeData(i2)"></div>
       <Lfoots></Lfoots>
     </div>
 
 </template>
 
 <script>
+  import storage from '../../router/storage';
     import Lfoots from "../TakeOut/Lfoots";
     export default {
         name: "Tsearch",
@@ -32,20 +49,116 @@
            //存所有餐馆数据
            datas:[],
            classbus:false,
-
+           baoqian:false,
+          address:"",
+           aaa:"",
+           urL:[],
+           // lishi:[],
+           // bbb:[]
          }
       },
       methods:{
+        doAdd(e){
+          console.log(this.urL);
+          //当按回车键的时候触发
+          // if(e.keyCode==13){
+          //   this.tijiao();
+          //   // 往urL数组里填输入框的值
+          //   this.urL.push({
+          //
+          //     title:this.aaa,
+          //
+          //   })
+          // }
+
+          storage.set('urL',this.urL);
+        },
+        removeData(k){
+
+          this.urL.splice(k,1);
+          storage.set("urL",this.urL)
+        },
+          sj(p){
+            console.log(p)
+          },
+        cz(){
+          // localStorage.setItem("username",);
+          
+        },
       tijiao(){
-        this.classbus=true
+          // console.log(this.aaa)
+
+
+        // localStorage.setItem("lishi",this.aaa)
+        // this.bbb.push(localStorage.getItem("lishi"))
+        // //这行向mutations传值
+        // this.$store.commit("lishi1",this.bbb)
+        //  console.log(this.bbb)
+        if(this.aaa!=""){
+          this.axios.get("https://elm.cangdu.org/v4/restaurants?geohash="+this.address.latitude+","+this.address.longitude+"&keyword="+this.aaa).then((p)=>{
+            // this.axios.get("https://elm.cangdu.org/v4/restaurants?geohash=31.22967,121.4762&keyword=肯德基").then((p)=>{
+            // console.log(p)
+            this.datas=p.data;
+            // console.log(this.datas)
+            if(this.datas.message=="搜索餐馆数据失败"||this.datas==""){
+              this.classbus=false;
+              this.baoqian=true;
+            }else {
+              this.classbus=true;
+              this.baoqian=false;
+            }
+            //这是输入框输入的值
+            console.log(this.aaa)
+           //    这是遍历的数据
+            console.log(this.urL)
+
+            // if(this.urL.indexOf(this.aaa)==-1){
+            console.log(this.urL);
+            // for(let i=0;i<this.urL.length;i++){
+               // console.log(this.urL[i].title.includes(this.aaa))
+               if(JSON.stringify(this.urL).indexOf(JSON.stringify(this.aaa))==-1) {
+
+                 this.urL.push({
+              title:this.aaa
+            })
+            storage.set('urL',this.urL);
+               //
+               }
+            console.log(JSON.stringify(this.urL).indexOf(JSON.stringify(this.aaa)) == -1);
+            // }
+            //  }
+            // }
+
+
+          });
+
+
+        }
+
       },
+
 
       },
       created(){
-        this.axios.get("https://elm.cangdu.org/v4/restaurants?geohash=31.22967,121.4762&keyword=肯德基").then((p)=>{
-          this.datas=p.data;
-          console.log(this.datas)
-        })
+        this.address = this.$store.state.address;
+        // this.lishi.push(this.$store.state.lishi);
+
+        // console.log(this.lishi);
+        // https://elm.cangdu.org/v4/restaurants?geohash=31.22967,121.4762&keyword=肯德基
+
+      },
+      mounted(){   /*生命周期函数       vue页面刷新就会触发的方法*/
+
+        this.urL=storage.get('urL');
+        // console.log(urL)
+        // for (let i = 0 ; i<urL.length;i++){
+        //   console.log(urL.indexOf(urL[i]))
+        // if(this.urL){  /*注意判断*/i
+        //   this.urL=this.urL;
+        //
+        //
+        //   }
+        // }
       },
       components: {Lfoots}
     }
@@ -59,6 +172,20 @@
 
   }
 
+.cha{
+  padding: 0 0.3rem;
+  font-size: 0.7rem;
+  height: 2rem;
+  border-bottom: 0.025rem solid #e4e4e4;
+  line-height: 2rem;
+  position: relative;
+}
+.cha img{
+position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+
+}
 
 #head{
 background: #3190e8;
@@ -73,17 +200,18 @@ background: #3190e8;
     margin-left: 0.4rem;
     margin-top:0.6rem;
   }
-  #head p{
-    display:inline-block;
+  #bt{
     font-size: 1rem;
+    margin-top: -1.8rem;
     text-align: center;
+    line-height: 2.2rem;
     color: #fff;
-    margin-left: 7rem;
+    /* margin-left: 7rem; */
     font-weight: 400;
   }
-  #input{
+  #inputs{
     background: #f2f2f2;
-    height: 1.5rem;
+    height: 1.7rem;
     width: 70%;
     border: 0.025rem solid #e4e4e4;
     border-radius: 0.125rem;
@@ -92,17 +220,23 @@ background: #3190e8;
     padding: 0.025rem;
     margin-left: 0.5rem;
     margin-top: 0.5rem;
+    margin-bottom: 0.4rem;
+    padding-left: 0.25rem;
+    font-size: 0.7rem;
   }
   #tj{
     background-color: #3190e8;
     border-radius: 0.125rem;
     color: white;
-    height: 1.6rem;
+    height: 1.7rem;
     width: 20%;
     border: 0.025rem solid #3190e8;
+    font-size: 0.75rem;
+    font-weight: 700;
   }
   #business{
-    height: 2rem;
+    height: 25rem;
+    overflow: auto;
     font-size: .6rem;
     line-height: 2rem;
     text-indent: .5rem;
@@ -110,5 +244,43 @@ background: #3190e8;
     background: #f5f5f5;
     color: #666;
     margin-top: 0.25rem;
+  }
+  #business li{
+    list-style-type:none;
+  }
+  #baoqian{
+    font-size: 0.65rem;
+    text-align: center;
+  }
+  .shangjia{
+    display: flex;
+    padding: 0.5rem;
+    border-bottom: 0.025rem solid #e4e4e4;
+    background: white;
+  }
+  .img1{
+
+    width: 1.8rem;
+    height: 1.8rem;
+
+  }
+  .xinxi{
+    width: 15.5rem;
+    padding-bottom: 0.5rem;
+    margin-bottom: 0.3rem;
+    border-bottom: 1px solid #e4e4e4;
+  }
+.xinxi>p:nth-child(1){
+  margin-top: -0.2rem;
+}
+  .p1{
+   height: 0.9rem;
+    line-height: 0.9rem;
+    font-weight: 400;
+    font-size: .65rem;
+
+  }
+  .p1 img{
+    margin-bottom: -0.1rem;
   }
 </style>
--- elemep/src/components/ytg/Tsearch2.vue
+++ elemep/src/components/ytg/Tsearch2.vue
@@ -1,54 +1,139 @@
+
 <template>
+
   <!--<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>-->
   <!--search是搜索-->
   <div id="search">
+
     <!--head是头部-->
     <div id="head">
-      <img src="../../assets/img_t/zuojian.png"/>
-      <p>数据</p>
-      <router-link :to="{}" id="qhcs">切换城市</router-link>
+      <img src="../../assets/img_t/zuojian.png" @click="back"/>
+      <div id="ll">{{address.name}}</div>
+
+      <router-link to="/Shouye" id="qhcs">切换城市</router-link>
     </div>
     <div >
-      <input type="text" placeholder="输入学校、商务楼、地址" id="input">
+      <input type="text" placeholder="输入学校、商务楼、地址" id="input" v-model="dizhi">
       <button id="tj" @click="tijiao()">提交</button>
     </div>
-    <div v-show="classbus" id="business">
-      <h4>商家</h4>
-      <router-link :to={} v-for="(p,i) in datas" :key="i">
-        {{p.name}}
-      </router-link>
+    <h5  id="business">搜索历史</h5>
+<ul>
+  <li  class="ss" v-for="(p1,i1) in dizhi2" v-if="true" :key="i1" @click="tzwm(i1)">
+    <p class="diyi">{{p1.name}}</p>
+    <p class="dier">{{p1.address}}</p>
+  </li>
+</ul>
+
+    <div>
+
+      <div >
+<ul>
+      <li  class="ss" v-for="(p,i) in lishi"  :key="i" @click="tzwm2(i)">
+        <p v-model="diyi" class="diyi">{{p.name}}</p>
+        <p v-model="dier" class="dier">{{p.address}}</p>
+      </li>
+      <li class="qk" v-if="qksy1" @click="qksy">清空所有</li>
+</ul>
+      </div>
     </div>
-    <Lfoots></Lfoots>
+    <!--<Lfoots></Lfoots>-->
   </div>
 
 </template>
 
 <script>
-  import Lfoots from "../TakeOut/Lfoots";
+  // import Lfoots from "../TakeOut/Lfoots";
+  import storage from '../../router/storage';
   export default {
     name: "Tsearch2",
     data(){
       return{
+        dizhi:"",
         //存所有餐馆数据
         datas:[],
         classbus:false,
-        abc:""
-
+        child1Msg:"",
+        address:"",
+        dizhi2:[],
+        diyi:"",
+        dier:"",
+        lishi:[],
+        qksy1:false
       }
     },
     methods:{
       tijiao(){
-        this.classbus=true
+        console.log(this.dizhi)
+        if(this.dizhi!=""){
+
+
+        this.classbus=true;
+        this.axios.get("https://elm.cangdu.org/v1/pois?city_id="+this.address.id+"&keyword="+this.dizhi+"&type=search").then((p) =>{
+          // console.log(this.address.id)
+          this.dizhi2=p.data;
+          console.log(p.data)
+        })
+      }
       },
+      tzwm(i){
+        this.lishi.push({
+          address:this.dizhi2[i].address,
+          name:this.dizhi2[i].name,
+          checked:false,
+        })
+
+        console.log(this.dizhi2[i].address)
+        console.log(this.dizhi2[i].name)
+
+        this.$router.push({
+          path: "/Lbody"
+        })
+        storage.set('lishi',this.lishi)
+      },
+      tzwm2(){
+        this.$router.push({
+          path: "/Lbody",
+
+        })
+      },
+     qksy(){
+        this.qksy1=false;
+       this.lishi.splice(0)
+       storage.set('lishi',this.lishi);
+
+
+     },
+      back(){
+        history.go(-1)
+      }
+    },
+    mounted(){   /*生命周期函数       vue页面刷新就会触发的方法*/
+
+      this.lishi=storage.get('lishi');
+      console.log(this.lishi);
+      if(this.lishi[0]){
+        this.qksy1=true
+      }else{
+        this.qksy1=false
+      }
+      // if(this.lishi){  /*注意判断*/
+      //   this.lishi=this.lishi;
+      //
+      // }
 
     },
     created(){
-      this.axios.get("https://elm.cangdu.org/v4/restaurants?geohash=31.22967,121.4762&keyword=肯德基").then((p)=>{
-        this.datas=p.data;
-        console.log(this.datas)
-      })
+      this.address = this.$store.state.address
+      console.log(this.lishi)
+     // if(this.lishi){
+     //
+     // }else {
+     //
+     // }
+
+
     },
-    components: {Lfoots}
+    // components: { Lfoots}
   }
 </script>
 
@@ -73,13 +158,16 @@
     /*position: absolute;*/
     margin-left: 0.4rem;
     margin-top:0.6rem;
+    opacity: ;
   }
-  #head p{
+  #ll{
     display:inline-block;
     font-size: 1rem;
+    line-height: 2.2rem;
     text-align: center;
     color: #fff;
-    margin-left: 7rem;
+    margin-left: 40%;
+    transform: translateX(-50%);
     font-weight: 400;
   }
   #input{
@@ -92,6 +180,7 @@
     padding: 0.025rem;
     margin-left: 10%;
     margin-top: 0.5rem;
+    font-size: 0.7rem;
   }
   #tj{
     background-color: #3190e8;
@@ -120,5 +209,35 @@
     position: absolute;
     top: 1rem;
     color: white;
+    text-decoration:none
+  }
+  .ss{
+
+    margin: 0 auto;
+    padding-top: .65rem;
+    border-bottom: 1px solid #e4e4e4;
+    background-color: #fff;
+    border-top: 1px solid #e4e4e4;
+  }
+  .diyi{
+    margin: 0 auto .35rem;
+    width: 90%;
+    font-size: .65rem;
+    color: #333;
+  }
+  .dier{
+    width: 90%;
+    margin: 0 auto .55rem;
+    font-size: .45rem;
+    color: #999;
+  }
+  .qk[data-v-259d3b6e] {
+    height: 2rem;
+    line-height: 2rem;
+    background: white;
+    text-align: center;
+  }
+  li{
+    list-style-type: none;
   }
 </style>
